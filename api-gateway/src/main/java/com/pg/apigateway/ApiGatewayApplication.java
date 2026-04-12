f9d6f9c260bebd5cd260e20643b52afc770eecd9
pg <prathamgupta050@gmail.com>
2026-04-13 00:03:57 +0530

    setup api-gateway all request will go via api-gateway to auth-service, closed port binding of auth-serivce it will no longer accept direction http requests

---
+++ api-gateway/src/main/java/com/pg/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.pg.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
+
+}
---
+++ auth-service/src/main/java/com/pg/authservice/dto/LoginResponseDTO.java
@@ -0,0 +1,14 @@
+package com.pg.authservice.dto;
+
+public class LoginResponseDTO {
+
+    private final String token;
+
+    public LoginResponseDTO(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
